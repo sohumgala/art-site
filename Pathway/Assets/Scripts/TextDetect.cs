75ce321ffc6713c3ce26de7368f1748f1fe5af38
albuskes <57105606+albuskes@users.noreply.github.com>
2020-10-17 20:21:06 -0400

    Added Location Screen and edited select screen

---
+++ Pathway/Assets/Scripts/TextDetect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextDetect : MonoBehaviour
+{
+	public string location;
+	public GameObject inputField;
+	public GameObject text;
+
+	public void StoreName(){
+		location = inputField.GetComponent<Text>().text;
+		text.GetComponent<Text>().text = "Finding art from " + location;
+	}
+
+}
+ 
---
+++ Pathway/Assets/Scripts/toMain.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toMain : MonoBehaviour
+{
+    public void changeMenuScene(string scenename){
+    	Application.LoadLevel(scenename);
+    }
+}
